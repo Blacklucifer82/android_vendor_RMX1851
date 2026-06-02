03c642a5bb1c329324153024d6cd1c5342e9f4c9
Hansraj76 <gamerx8x6x@gmail.com>
2026-06-02 20:07:04 +0530

    RMX1851:sync with DT

--- RMX1851-vendor.mk
+++ RMX1851-vendor.mk
@@ -295,7 +295,6 @@ PRODUCT_PACKAGES += \
     libGLESv2_adreno \
     libq3dtools_adreno \
     libq3dtools_esx \
-    android.hardware.health@2.0-impl-2.1-qti \
     vulkan.adreno \
     libC2D2 \
     libCB \
